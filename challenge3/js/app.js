792538419273b2766c95eaa3218facc55ca691cf
Grant Evans <grant.evans300@gmail.com>
2016-10-26 12:09:38 -0700

    added alex's starter code

--- challenge3/js/app.js
+++ challenge3/js/app.js
@@ -7,7 +7,10 @@ add your code to this file
 var dropdown = document.querySelector("#report-select");
 var table = document.querySelector(".table");
 
-var modMOVIES = MOVIES;
+var starWarsB = false;
+var twentyB = false;
+var avgSalesB = false;
+var top100B = false;
 
 // Build the filtered lists of names.
 // Array.filter allows you to take an array of items
@@ -24,11 +27,21 @@ starWars.sort(function(a, b) {
     return a.title.localeCompare(b.title);
 });
 
+
+
 //console.log(starWars);
 
 var twenty = MOVIES.filter(function (item) {
-    return item.released.toLowerCase() === "female";
+    return item.released < 2000-01-01;
 });
+
+var avgSales = MOVIES.reduce(function (previousValue, currentValue){
+    return previousValue.sales + currentValue.sales;
+});
+
+var top100 = MOVIES
+
+
 
 
 
@@ -36,6 +49,9 @@ function buildTable() {
     // table body and table head
     var tbody = document.createElement("tbody");
     var thead = document.createElement("thead");
+
+    var h2 = document.createElement("h2");
+
 
     // Row for the header
     var threadRow = document.createElement("tr");
@@ -78,17 +94,85 @@ function buildTable() {
     thead.appendChild(threadRow);
     table.appendChild(tbody);
     table.appendChild(thead);
+
+    report.appendChild(table);
 }
 
+function buildTableSales() {
+    // table body and table head
+    var tbody = document.createElement("tbody");
+    var thead = document.createElement("thead");
+
+    // Row for the header
+    var threadRow = document.createElement("tr");
+
+    // Columns for the header
+    
+    var genreTh = document.createElement("th");
+    genreTh.textContent = "Genre";
+
+    var salesTh = document.createElement("th");
+    salesTh.textContent = "Sales";
+
+    // Append these elements to the table
+    threadRow.appendChild(genreTh);
+    threadRow.appendChild(salesTh);
+
+    thead.appendChild(threadRow);
+    table.appendChild(tbody);
+    table.appendChild(thead);
+
+    report.appendChild(table);
+
+}
+
+function buildTableTop() {
+    // table body and table head
+    var tbody = document.createElement("tbody");
+    var thead = document.createElement("thead");
+
+    // Row for the header
+    var threadRow = document.createElement("tr");
+
+    // Columns for the header
+    
+    var titleTh = document.createElement("th");
+    titleTh.textContent = "Title";
+
+    var ticketsTh = document.createElement("th");
+    ticketsTh.textContent = "Ticket Sales";
+
+    // Append these elements to the table
+    threadRow.appendChild(titleTh);
+    threadRow.appendChild(ticketsTh);
+
+    thead.appendChild(threadRow);
+    table.appendChild(tbody);
+    table.appendChild(thead);
+
+}
 
 function buildRows(rows) {
+    console.log(rows);
 
-    buildTable();
+    table.innerHTML = "";
+
+    if (avgSalesB){
+        buildTableSales();
+    }else if (top100B){
+        buildTableTop();
+    } else {
+        buildTable();
+    }
 
     var tbody = document.querySelector("tbody");
 
+    
+
     rows.forEach(function (title) { //titles
         var titleTr = document.createElement("tr");
+        
+        
 
         // Object.keys returns an array of the keys object
         var titleKeys = Object.keys(title);
@@ -101,8 +185,10 @@ function buildRows(rows) {
 
             var td = document.createElement("td");
             td.textContent = value;
+            
 
             titleTr.appendChild(td);
+
         });
 
         tbody.appendChild(titleTr);
@@ -113,7 +199,7 @@ function buildRows(rows) {
 // we want to clear and rebuild the table
 // based on the selected gender.
 dropdown.addEventListener("change", function (e) {
-    // Removes all the elements in the able.
+    // Removes all the elements in the table.
     table.innerHTML = "";
 
     // Get the current value of the dropdown,
@@ -121,10 +207,89 @@ dropdown.addEventListener("change", function (e) {
     var value = e.target.value;
 
     if (value === "star-wars") {
-        buildRows(starWars);
-    } else {
+        starWarsB = true;
+        twentyB = false;
+        avgSalesB = false;
+        top100B = false;
+        //buildRows(starWars);
+        buildTable2(starWars);
+    }else if (value === "20th"){
+        starWarsB = false;
+        twentyB = true;
+        avgSalesB = false;
+        top100B = false;
+        buildRows(twenty);
+    } else if (value === "avg-by-genre"){
+        starWarsB = false;
+        twentyB = false;
+        avgSalesB = true;
+        top100B = false;
+        buildRows(avgSales);
+    } else if (value === "top-by-tikets"){
+        starWarsB = false;
+        twentyB = false;
+        avgSalesB = false;
+        top100B = true;
+        buildRows(top100);
+    }else {
         buildRows(MOVIES);
     }
 });
 
 buildRows(MOVIES);
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    //make sure js loads
+    console.log("doc ready");
+    //check global var
+    console.log(MOVIES);
+
+    var table = document.querySelector(".table");
+
+    var buildTable2 = function(records) {
+        console.log(records);
+
+        table.innerHTML = "";
+
+        var tbody = document.createElement("tbody");
+        var thead = document.createElement("thead");
+
+        var columns = [] // fill with column names from records[0]
+
+        var th = document.createElement("th");
+        //for each column make a td and append it to the th
+
+        Object.keys(records).forEach(function(column){
+            var td = document.createElement("td");
+            th.AppendChild(td);
+        })
+
+        records.forEach(function(record) {
+            // make a tr
+            var tr = document.createElement("tr");
+            columns.forEach(function(column) {
+                // make a td and fill with data from record[column];
+                // append to tr
+                var td = document.createElement("td");
+
+                var value = record[column];
+                td.textContent(value);
+
+                tr.AppendChild(td);
+
+            });
+
+            // append tr to tbody;
+            tbody.AppendChild(tr);
+        })
+
+        //append thead and tbody to table;
+        table.appendChild(thead);
+        table.appendChild(tbody);
+    }
+
+
+    // TESTER code
+    buildTable(MOVIES.slice(0, 20));
+});
