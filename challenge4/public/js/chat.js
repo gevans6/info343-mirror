1d6056cba74a11c0a1b8108d553e8d576e8dfeb7
Grant Evans <grant.evans300@gmail.com>
2016-11-06 19:09:26 -0800

    began edit post functionality

--- challenge4/public/js/chat.js
+++ challenge4/public/js/chat.js
@@ -34,11 +34,44 @@ firebase.auth().onAuthStateChanged(function(user) {
             var img = document.createElement("img");
             img.src = message.photoURL;
 
+            
+
             var nameHeader = document.createElement("h4");
             nameHeader.innerText = message.displayName;
 
             var messageContent = document.createElement("p");
+            messageContent.id = "original-post"
             messageContent.innerText = message.text;
+
+            var editButton = document.createElement("button");
+            editButton.id = "edit-button";
+            editButton.className = "btn btn-link";
+            editButton.innerText = "Edit";
+            editButton.type = "click";
+            
+            var removeButton = document.createElement("button");
+            removeButton.id = "remove-button";
+            removeButton.className = "btn btn-link";
+            removeButton.innerText = "Delete";
+            
+            var editTextArea = document.createElement("textarea");
+            editTextArea.id = "edit-box";
+            editTextArea.rows = "10";
+            editTextArea.innerText = messageContent.innerText;
+            editTextArea.classList.add("hide");
+
+            var editConfirmButton = document.createElement("button");
+            editConfirmButton.id = "edit-confirm";
+            editConfirmButton.className = "btn btn-primary";
+            editConfirmButton.innerText = "Confirm Edit";
+            editConfirmButton.classList.add("hide");
+
+            var editCancelButton = document.createElement("button");
+            editCancelButton.id = "edit-cancel";
+            editCancelButton.className = "btn btn-default";
+            editCancelButton.innerText = "Cancel";
+            editCancelButton.classList.add("hide");
+
 
             var messageLi = document.createElement("li");
             
@@ -47,15 +80,28 @@ firebase.auth().onAuthStateChanged(function(user) {
             messageLi.appendChild(nameHeader);
             messageLi.appendChild(messageContent);    
             messageLi.appendChild(img);
+        
+            messageLi.appendChild(editButton);
+            messageLi.appendChild(removeButton);
+            messageLi.appendChild(editTextArea);
+            messageLi.appendChild(editConfirmButton);
+            messageLi.appendChild(editCancelButton);
             
             
-            //messageLi.className = "";
+            editButton.addEventListener("click", function(e) {
+                editTextArea.classList.toggle("hide");
+                editConfirmButton.classList.toggle("hide");
+                editCancelButton.classList.toggle("hide");
+            });
 
-            
-            
+            editConfirmButton.addEventListener("submit", function(e){
+                // change href of the text for the message
+                editTextArea.classList.toggle("hide");
+            })
+
             messagesList.appendChild(messageLi);
 
-            
+            console.log(user.getToken());          
 
             });
 
@@ -105,5 +151,3 @@ messageForm.addEventListener("submit", function(e) {
 
     });
 });
-
-
